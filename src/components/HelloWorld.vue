8aa82cb26de1a179f3f1c34bee228099eeda0cda
sickb0i <skodak95alexis@gmail.com>
2021-07-22 10:10:09 +0200

    [feat]: add user display card

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,19 @@
-<script>
-import { defineComponent } from '@vue/composition-api'
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>{{user.name}}</v-card-title>
+      <v-card-subtitle>{{user.lastname}}</v-card-subtitle>
+    </v-card>
+  </v-container>
+</template>
 
-export default defineComponent({
-  setup() {
-    
-  },
-})
+<script>
+export default {
+  data: () => ({
+    user: {
+      name: 'John',
+      lastname: 'Doe'
+    }
+  })
+}
 </script>
